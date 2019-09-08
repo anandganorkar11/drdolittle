665eab5b80fa44b3783ea59157d8a2835987c6a1
anandganorkar11 <anandganorkar11@gmail.com>
2019-09-08 15:03:55 +0530

    Update ServletInitializer.java

--- service/src/main/java/com/drdolittle/petclinic/petclinic/ServletInitializer.java
+++ service/src/main/java/com/drdolittle/petclinic/petclinic/ServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-//comment added
+//removed comment
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(PetClinicApplication.class);
